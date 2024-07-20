9f6baf69067e12c86355e377e2abb1bd51b35927
ZEINZH <91441817+suryanicholas@users.noreply.github.com>
2024-07-20 14:31:33 +0700

    uwu

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -17,7 +17,11 @@
             }
 
             main.contents{
-                scroll-behavior: smooth;
+                scroll-snap-type: y mandatory;
+            }
+
+            main.contents .container{
+                scroll-snap-align: start
             }
 
             .welcome.container h1{
@@ -55,58 +59,8 @@
                     </div>
                 </div>
             </header>
-            <main class="contents flex-fill overflow-y-auto">
-                {{-- <div class="welcome container h-100 d-flex align-items-center justify-content-center">
-                    <div class="row">
-                        <div class="col text-center">
-                            <h1>Selamat Datang</h1>
-                            <a href="#identifyForm" class="btn btn-primary p-1 mt-3">Mulai Evaluasi</a>
-                            <div class="small mt-1 text-secondary">
-                                <span class="material-symbols-outlined">expand_circle_down</span>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
-                <div id="identifyForm" class="container h-100">
-                    <div class="position-relative row h-100 align-items-center">
-                        <div class="col-6 mx-auto position-relative z-0">
-                            <div class="mb-3 fs-5 text-center">
-                                <span>Kami perlu mengidentifikasi diri anda sebelum melanjutkan</span>
-                            </div>
-                            <hr>
-                            <form class="mb-3">
-                                <div class="input-group">
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control" name="identify" id="identify" maxlength="18" required placeholder="Masukkan nomor identitas anda...">
-                                        <label for="identify">Nomor Identitas</label>
-                                    </div>
-                                    <button class="btn btn-primary d-flex align-items-center" type="submit">
-                                        <span class="material-symbols-outlined">person_search</span>
-                                    </button>
-                                </div>
-                                <small class="mt-1 text-warning d-flex align-items-center">
-                                    <span class="material-symbols-outlined me-1">error</span>
-                                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde?</span>
-                                </small>
-                            </form>
-                            <small>
-                                <div class="fw-bold">Catatan</div>
-                                <div class="">
-                                    <ul>
-                                        <li>Masukkan Nomor Induk Siswa Nasional (NISN) bagi anda yang merupakan seorang siswa/i.</li>
-                                        <li>Untuk anda yang merupakan seorang Guru/Staff/Pegawai/Kepala Sekolah, gunakan Nomor Induk Pegawai (NIP).</li>
-                                    </ul>
-                                </div>
-                            </small>
-                        </div>
-                        <div class="position-absolute col-12 h-100 z-1 mx-auto bg-light">
-                            
-                        </div>
-                    </div>
-                </div>
-            </main>
+            @yield('contents')
         </div>
-        {{-- @yield('contents') --}}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         @yield('scripts')
